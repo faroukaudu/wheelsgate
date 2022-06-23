a4a553cb86edafaec282d0f1656dd42719296723
Farouk Audu <fagzy99@gmail.com>
2022-06-23 14:16:15 -0700

    done with the about us page
    Remaining the styling

---
+++ about.php
@@ -0,0 +1,236 @@
+<?php
+    include("include/header.php");
+?>
+        <!-- Header Section -->
+
+
+        <!-- Main Content Section -->
+        <main class="main">
+            <!-- Page Title -->
+            <div class="page-title text-center">
+                <h2 class="title"> The Company </h2>
+                <p class="description light"> Holisticly brand sustainable solutions rather than clicks-and-mortar applications.
+                    <br> Phosfluorescently whiteboard fully tested initiatives. </p>
+            </div>
+            <!-- Page Title -->
+            <!-- Breadcrumbs -->
+            
+            <div class="breadcrumbs">
+                <div class="container">
+                    <span class="parent"> <i class="fa fa-home"></i> <a href="index.html"> Home </a> </span>
+                    <i class="fa fa-chevron-right"></i>
+                    <span class="child"> <a href="about.html"> About </a> </span>
+                    <i class="fa fa-chevron-right"></i>
+                    <span class="child "> COMPANY OVERVIEW </span>
+                </div>
+            </div>
+
+            <div class="container">
+
+                <div class="row about-sidebar">
+
+                    <div class="col-md-9 about-content">
+                    <section class="about-culture">
+                            <div class="row culture">
+                            <h2 class="culture-title2"> THE COMPANY </h2>
+                                <p>Wheelsgate Engineering Services Limited (WESL) is a specialist Pipeline Engineering Consultancy
+                                        company. WESL is a fully owned Nigerian company. We provide a wide range of high-quality
+                                        technical and management services to offshore and onshore oil and gas companies in the areas
+                                        of:</p>
+                                <div class="col-sm-6 cul">
+                                    <ul style="list-style: square;padding-left: 25px; font: 15px;">
+                                        <li>Pipeline and asset integrity</li>
+                                        <li>Process and pipeline precommisioning services</li>
+                                        <li>Technology and engineering</li>
+                                        <li>Valve services and maintenance</li>
+                                        <li>Project management</li>
+                                        <li>Training.</li>
+                                    </ul>
+                                </div>
+                                <div class="col-sm-6 space-image">
+                                    <a href="#" class="hover-effect">
+                                        <img src="images/services-two.jpg" alt="our-operations" />
+                                    </a>
+                                </div>
+                                <p>As a national company based in Nigeria, with global experience, we are exceptionally positioned
+                                        to serve both the Nigerian and global markets within the energy industry. We offer flexible
+                                        consultancy solutions, customized to the needs of our clients. As we continue to develop new
+                                        asset engineering and integrity solutions and explore different markets, one thing will continue
+                                        to remain constant; WESL will never compromise its commitment to providing excellent services
+                                        to the highest quality possible.</p>
+
+                                        <h2 class="culture-title2"> VISION </h2>
+                                        <P>To help Operating Companies and Pipeline Operators to operate their pipeline assets safely and
+                                            reliably.</P>
+
+
+                                            <h2 class="culture-title2"> MISSION </h2>
+                                        <P>WESL’s mission is to support our clients in their pipeline assets management efforts through
+                                            provision of accurate, reliable, verifiable, and cost-effective engineering solutions based on
+                                            industry best practice.</P>
+
+                                            <h2 class="culture-title2"> OUR VALUES </h2>
+                                            <div class="col-sm-12 cul">
+                                                <ul style="list-style: square;padding-left: 25px; font: 15px;">
+                                                    <li>We are passionate in how we support our Clients.</li>
+                                                    <li>We are responsive to client needs.</li>
+                                                    <li>We are committed to efficient and effective delivery of clients’ projects to the highest quality possible</li>
+                                                    <li>We value teamwork, innovation, and continuous improvement.</li>
+                                                    <li>We believe in investing in our employees</li>
+                                                    <li>We believe in giving to our Community.</li>
+                                                    <li>We respect our Clients, Partners, Sub-contractors, and Each Other.</li>
+                                                    <li>We strive to deliver excellence.</li>
+                                                    <li>Promote safe, honest, and ethical conduct</li>
+                                                </ul>
+                                            </div>
+                                <div class="row culture">
+                                            <h2 class="culture-title2"> OUR Approach </h2>
+                                <div class="col-sm-6 cul">
+                                    
+                    
+                                    <p> We believe that great collaborations produce great results. We employ a hands-on approach to
+                                        every task because we recognize that each task is different, and that client needs vary. We work
+                                        in close collaboration with our clients through all phases of their projects. This allows us to tailor
+                                        our solutions to clients’ unique needs. We assign dedicated project managers to each project to
+                                        ensure that projects are implemented with appropriate resources and in accordance to project
+                                        schedule and budget.</p>
+                                </div>
+                                <div class="col-sm-6 culture-video">
+                                    <a href="#" class="hover-effect"> <img src="images/emp.jpg" alt="employee" /> </a>
+                                </div>
+
+                            </div>
+                            <h2 class="culture-title2"> WHY CHOOSE US </h2>
+                                        <P>Reliable * Committed * Experienced Staff * Integrity * Competitive rates * High quality services</P>
+
+                                        <h2 class="culture-title2"> OUR TEAM </h2>
+                                        <P>We have an excellent team of professionals with vast experience in pipeline engineering with
+                                            over 120 years of combined experience in the energy sector. Our experts have supported many
+                                            operators in Nigeria and around the world in their pipeline integrity, process and pipeline
+                                            precommisioning efforts.</P>
+
+
+                                            <div class="row culture">
+                                            <h2 class="culture-title2"> Foreign Partners </h2>
+                                <div class="col-sm-6 cul">
+                                    
+                    
+                                    <p> While the core expertise of WESL is in Pipeline Integrity and process &amp; Pipeline Precommisioning
+                                            services, we also work with The Jukes Group (TJG), our foreign technical partner to provide a
+                                            broader range of engineering services to the energy industry. The Jukes Group is a global
+                                            company based in Houston, USA, with over 650 Subject Matter Experts in the Energy Sector.
+                                            Experts at TJG have capabilities in a wide range of services in the areas of Technology &amp;
+                                            Engineering, Leadership &amp; Motivation, Education &amp; Teaching, and Research &amp; Development.</p>
+                                </div>
+                                <div class="col-sm-6 culture-video">
+                                    <a href="#" class="hover-effect"> <img src="images/emp.jpg" alt="employee" /> </a>
+                                </div>
+
+                            </div>
+
+                            <h2 class="culture-title2"> Corporate Responsibility </h2>
+                                        <P>At Wheelsgate, we are committed to being a good corporate citizen. Our commitment to
+                                            responsible Corporate Social Responsibility (CSR) defines our dedication to accountability,
+                                            transparency, ethical business practices, environmental stewardship, and to making positive
+                                            impacts within the entire scope of our business footprint.</P>
+
+                                            
+                                
+                            </div>
+                            <hr class = "my-hr">
+
+                            
+    
+
+                        </section>
+                        
+
+
+
+
+
+                       
+                    
+
+
+
+                    </div>
+
+                    <div class="col-md-3 sidebar left">
+
+                        <div class="sidebar-blog-categories">
+                            <ul>
+                                <li> <a href="company.html"> The Company </a> </li>
+                                <li> <a href="mission.html"> Our Vision &amp; Mission </a> </li>
+                                <li> <a href="innovation.html"> Innovation </a> </li>
+                                <li> <a href="approach.html"> Our Approach </a> </li>
+                                <li> <a href="leaders.html"> Our Leaders </a> </li>
+                            </ul>
+                        </div>
+
+                        <div class="sidebar-download">
+                            <div class="download-file color-bg">
+                                <a href="#"><i class="fa fa-file-pdf-o" aria-hidden="true"></i>  Download PDF <span>1.5 MB</span>
+                                </a>
+                            </div>
+
+                        </div>
+
+                        <div class="sidebar-fact">
+
+                            <h3 class="about-quick-fact">Quick Fact</h3>
+                            <p>Distinctively fashion in the effect of standard products communities via seamless applications. Authoritatively recaptiualize.</p>
+                            <a href="#" class="btn btn-primary about-quick-fact-button" role="button">CALL TO ACTION</a>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </main>
+        <!-- Main Content Section -->
+
+        <!-- Footer Section -->
+        <?php
+    include("include/footer.php");
+?>
+
+        <!-- Footer Section -->
+
+        <!-- back-to-top link -->
+        <a href="#0" class="cd-top"> Top </a>
+
+    </div>
+    <!-- Page Wrapper
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+    <!-- Javascripts
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+    <!-- Jquery Library -->
+    <script src="js/jquery.min.js"></script>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Counter JS -->
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+
+    <!-- Back to top -->
+    <script src="js/back-to-top.js"></script>
+
+    <!-- Form Validation -->
+    <script src="js/validate.js"></script>
+
+    <!-- Subscribe Form JS -->
+    <script src="js/subscribe.js"></script>
+
+    <!-- Main JS -->
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
